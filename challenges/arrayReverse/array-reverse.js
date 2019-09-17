74f0174ad58626c91cb85be47aa6c6a4e52c4ac6
allisonbusse <allisonbusse@gmail.com>
2019-09-17 13:35:24 -0700

    fixed error

--- challenges/arrayReverse/array-reverse.js
+++ challenges/arrayReverse/array-reverse.js
@@ -1,10 +1,11 @@
+// eslint-disable-next-line no-unused-vars
 function reverseArray(arr) {
-    let array = [];
-    let index = 0;
-    for(let i = arr.length - 1; i >= 0; i-- ) {
-        array[index] = input[i];
-        index += 1;
-    }
-    return array;
+  let array = [];
+  let index = 0;
+  for(let i = arr.length - 1; i >= 0; i-- ) {
+    array[index] = arr[i];
+    index += 1;
+  }
+  return array;
 }
 
