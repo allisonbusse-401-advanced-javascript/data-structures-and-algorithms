133b4e9ce392f6a3490f790935e5f15baa2c5da6
allisonbusse <allisonbusse@gmail.com>
2019-11-02 16:01:00 -0700

    wrote class and methods and tests

---
+++ challenges/hashtable/hashtable.test.js
@@ -0,0 +1,22 @@
+const HashTable = require('./hashtable');
+
+
+describe('Hash Table Tests', () => {
+  it('hashes a key', () => {
+    const testTable = new HashTable();
+    expect(testTable.hash('allison')).toBe(82);
+  });
+
+  it('sets a key value pair', () => {
+    const testTable = new HashTable();
+    testTable.set('allison', 'busse');
+    expect(testTable.get('allison')).toBe('busse');
+  });
+
+  it('sets a key value pair when the key already exists', () => {
+    const testTable = new HashTable();
+    testTable.set('allison', 'smith');
+    expect(testTable.get('allison')).toBe('smith');
+  });
+});
+
